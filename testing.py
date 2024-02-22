2ca94c0db0676d8445c6c65a9090a8e253909cc5
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2024-02-22 16:26:19 +0530

    Counting cars only

--- testing.py
+++ testing.py
@@ -1,7 +1,9 @@
+import os
 import cv2
 import pandas as pd
+import numpy as np
 from ultralytics import YOLO
-import os
+from tracker import *
 
 model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'best.pt')
 model = YOLO(model_path)
@@ -25,6 +27,10 @@ print(class_list)
 
 count = 0
 
+area = [(295, 268), (476, 280), (468, 300), (278, 280)]
+tracker = Tracker()
+area_c = set()
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -39,6 +45,8 @@ while True:
     a = results[0].boxes.data
     px = pd.DataFrame(a).astype("float")
 
+    list = []
+
     for index, row in px.iterrows():
         x1 = int(row[0])
         y1 = int(row[1])
@@ -46,9 +54,29 @@ while True:
         y2 = int(row[3])
         d = int(row[5])
         c = class_list[d]
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0), 1)
 
+        if 'car' in c:
+            list.append([x1, y1, x2, y2])
+
+    bbox_id = tracker.update(list)
+
+    for bbox in bbox_id:
+        x3, y3, x4, y4, id = bbox
+        cx = int(x3 + x4) // 2
+        cy = int(y3 + y4) // 2
+
+        result = cv2.pointPolygonTest(np.array(area, np.int32), (cx, cy), False)
+        if result >= 0:
+            cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
+            area_c.add(id)
+
+        cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 0, 255), 2)
+        cv2.putText(frame, str(id), (x3, y3), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 0), 1)
+
+    cv2.polylines(frame, [np.array(area, np.int32)], True, (255, 25, 0), 2)
+    count = len(area_c)
+    # print(len(area_c))
+    cv2.putText(frame, str(count), (50, 80), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 255), 2)
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
